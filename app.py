5485cfc487aba58bf589255958c235194f15fe54
funkyremi <remi.testa@gmail.com>
2020-10-14 20:20:19 +0100

    Improvement

--- app.py
+++ app.py
@@ -11,16 +11,13 @@ app = Flask(__name__)
 
 @app.route('/rotate')
 def rotate():
-    try:
-        number = float(request.args.get('number'))
-        rotations_nb = round(number * 512)
-        if rotations_nb < 0:
-            motor.motor_run(GpioPins , .001, -rotations_nb, False, False, "half", .05)
-        elif rotations_nb >= 0:
-            motor.motor_run(GpioPins , .001, rotations_nb, True, False, "half", .05)
-        return 'Done'
-    except Exception as e:
-        return 'Failed: ' + e
+    number = float(request.args.get('number'))
+    rotations_nb = round(number * 512)
+    if rotations_nb < 0:
+        motor.motor_run(GpioPins , .001, -rotations_nb, False, False, "half", .05)
+    elif rotations_nb >= 0:
+        motor.motor_run(GpioPins , .001, rotations_nb, True, False, "half", .05)
+    return 'Done'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
