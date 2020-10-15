e9779237a88c91e2e57703c0cb6739829f0d22e3
funkyremi <remi.testa@gmail.com>
2020-10-15 18:00:50 +0100

    Get and save state

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
 from flask import Flask, request
+app = Flask(__name__)
 
-# Setting the motor
+# Setup the motor
 GpioPins = [18, 23, 24, 25]
 motor = RpiMotorLib.BYJMotor("Motor", "28BYJ")
 
-# Init app
-app = Flask(__name__)
+# Set and get state from file
+def get_state():
+    state_file = open("state.txt", "r")
+    state = state_file.read()
+    state_file.close()
+    return state
+
+def set_state(state):
+    state_file = open("state.txt", "w")
+    state_file.write(state)
+    state_file.close()
 
 @app.route('/rotate')
 def rotate():
@@ -15,9 +25,15 @@ def rotate():
     rotations_nb = round(number * 512)
     if rotations_nb < 0:
         motor.motor_run(GpioPins , .001, -rotations_nb, False, False, "half", .05)
+        set_state("0")
     elif rotations_nb >= 0:
         motor.motor_run(GpioPins , .001, rotations_nb, True, False, "half", .05)
+        set_state("1")
     return 'Done'
+
+@app.route('/state')
+def state():
+    return get_state()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
