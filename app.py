6a84856b6bf8521f2f2ca028a0ad37e5dea5b992
funkyremi <remi.testa@gmail.com>
2020-11-09 19:30:08 +0100

    Improve motor management

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
-import threading
 from RpiMotorLib import RpiMotorLib
 from flask import Flask, request
 app = Flask(__name__)
+
+is_moving = False
 
 # Setup the motor
 GpioPins = [18, 23, 24, 25]
@@ -20,23 +21,30 @@ def set_state(state):
     state_file.write(state)
     state_file.close()
 
-def open_door(rotations_nb):
-    motor.motor_run(GpioPins , .0005, -rotations_nb, False, False, "half", .05)
-
-def close_door(rotations_nb):
-    motor.motor_run(GpioPins , .005, rotations_nb, True, False, "half", .05)
-
 @app.route('/rotate')
 def rotate():
-    number = float(request.args.get('number'))
-    rotations_nb = round(number * 512)
-    if rotations_nb < 0:
-        threading.Thread(target=open_door, args=[rotations_nb]).start()
-        set_state("0")
-    elif rotations_nb >= 0:
-        threading.Thread(target=close_door, args=[rotations_nb]).start()
-        set_state("1")
-    return 'done'
+    global is_moving
+    if not is_moving:
+        number = float(request.args.get('number'))
+        rotations_nb = round(number * 512)
+        if rotations_nb < 0:
+            print('Opening')
+            is_moving = True
+            motor.motor_run(GpioPins , .0005, -rotations_nb, False, False, "half", .05)
+            is_moving = False
+            print('Opened')
+            set_state("0")
+            return 'Door opened'
+        elif rotations_nb >= 0:
+            print('Closing')
+            is_moving = True
+            motor.motor_run(GpioPins , .005, rotations_nb, True, False, "half", .05)
+            is_moving = False
+            print('Closed')
+            set_state("1")
+            return 'Door closed'
+    else:
+        return 'Please wait until the motor has stopped moving.'
 
 @app.route('/state')
 def state():
@@ -44,4 +52,3 @@ def state():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-
